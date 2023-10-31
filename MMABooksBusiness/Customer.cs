2f9bd83491f585043160c1ba994a5fd1b0a2536b
woodaa <woodaa@my.lanecc.edu>
2023-10-31 10:39:59 -0700

    Customer.cs class complete?

--- MMABooksBusiness/Customer.cs
+++ MMABooksBusiness/Customer.cs
@@ -1,10 +1,239 @@
-﻿using System;
+﻿using MMABooksDB;
+using MMABooksProps;
+using MMABooksTools;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Text;
 
 namespace MMABooksBusiness
 {
-    public class Customer
+    public class Customer : BaseBusiness
     {
+        public int CustomerID
+        {
+            get
+            {
+                return ((CustomerProps)mProps).CustomerID;
+            }
+
+            set
+            {
+                if (!(value == ((CustomerProps)mProps).CustomerID))
+                {
+                    if (value >= 0)
+                    {
+                        mRules.RuleBroken("CustomerID", false);
+                        ((Customer)mProps).CustomerID = value;
+                        mIsDirty = true;
+                    }
+
+                    else
+                    {
+                        throw new ArgumentOutOfRangeException("That customer ID no workie.");
+                    }
+                }
+            }
+        }
+
+        public String Name
+        {
+            get
+            {
+                return ((CustomerProps)mProps).Name;
+            }
+
+            set
+            {
+                if (!(value == ((CustomerProps)mProps).Name))
+                {
+                    if (value.Trim().Length >= 1 && value.Trim().Length <= 20)
+                    {
+                        mRules.RuleBroken("Name", false);
+                        ((Customer)mProps).Name = value;
+                        mIsDirty = true;
+                    }
+
+                    else
+                    {
+                        throw new ArgumentOutOfRangeException("Name must be no more than 20 characters long.");
+                    }
+                }
+            }
+        }
+
+        public String Address
+        {
+            get
+            {
+                return ((CustomerProps)mProps).Address;
+            }
+
+            set
+            {
+                if (!(value == ((CustomerProps)mProps).Address))
+                {
+                    if (value.Trim().Length >= 1 && value.Trim().Length <= 35)
+                    {
+                        mRules.RuleBroken("Address", false);
+                        ((Customer)mProps).Address = value;
+                        mIsDirty = true;
+                    }
+
+                    else
+                    {
+                        throw new ArgumentOutOfRangeException("Address must be no more than 35 characters long.");
+                    }
+                }
+            }
+        }
+        public String City
+        {
+            get
+            {
+                return ((CustomerProps)mProps).City;
+            }
+
+            set
+            {
+                if (!(value == ((CustomerProps)mProps).City))
+                {
+                    if (value.Trim().Length >= 1 && value.Trim().Length <= 100)
+                    {
+                        mRules.RuleBroken("City", false);
+                        ((Customer)mProps).City = value;
+                        mIsDirty = true;
+                    }
+
+                    else
+                    {
+                        throw new ArgumentOutOfRangeException("City must be no more than 100 characters long.");
+                    }
+                }
+            }
+        }
+        public String State
+        {
+            get
+            {
+                return ((CustomerProps)mProps).State;
+            }
+
+            set
+            {
+                if (!(value == ((CustomerProps)mProps).State))
+                {
+                    if (value.Trim().Length >= 1 && value.Trim().Length <= 35)
+                    {
+                        mRules.RuleBroken("State", false);
+                        ((Customer)mProps).State = value;
+                        mIsDirty = true;
+                    }
+
+                    else
+                    {
+                        throw new ArgumentOutOfRangeException("Name must be no more than 20 characters long.");
+                    }
+                }
+            }
+        }
+        public String ZipCode
+        {
+            get
+            {
+                return ((CustomerProps)mProps).ZipCode;
+            }
+
+            set
+            {
+                if (!(value == ((CustomerProps)mProps).ZipCode))
+                {
+                    if (value.Trim().Length >= 1 && value.Trim().Length <= 20)
+                    {
+                        mRules.RuleBroken("ZipCode", false);
+                        ((Customer)mProps).ZipCode = value;
+                        mIsDirty = true;
+                    }
+
+                    else
+                    {
+                        throw new ArgumentOutOfRangeException("ZipCode must be no more than 20 characters long.");
+                    }
+                }
+            }
+        }
+        public override object GetList()
+        {
+            List<Customer> customers = new List<Customer>();
+            List<CustomerProps> props = new List<CustomerProps>();
+
+
+            props = (List<CustomerProps>)mdbReadable.RetrieveAll();
+            foreach (CustomerProps prop in props)
+            {
+                Customer c = new Customer(prop);
+                customers.Add(c);
+            }
+
+            return customers;
+        }
+
+        protected override void SetDefaultProperties()
+        {
+        }
+
+        /// <summary>
+        /// Sets required fields for a record.
+        /// </summary>
+        protected override void SetRequiredRules()
+        {
+            mRules.RuleBroken("CustomerID", true);
+            mRules.RuleBroken("Name", true);
+            mRules.RuleBroken("Address", true);
+            mRules.RuleBroken("City", true);
+            mRules.RuleBroken("State", true);
+            mRules.RuleBroken("ZipCode", true);
+        }
+
+        /// <summary>
+        /// Instantiates mProps and mOldProps as new Props objects.
+        /// Instantiates mbdReadable and mdbWriteable as new DB objects.
+        /// </summary>
+        protected override void SetUp()
+        {
+            mProps = new CustomerProps();
+            mOldProps = new CustomerProps();
+
+            mdbReadable = new CustomerDB();
+            mdbWriteable = new CustomerDB();
+        }
+
+        #region constructors
+        /// <summary>
+        /// Default constructor - gets the connection string - assumes a new record that is not in the database.
+        /// </summary>
+        public Customer() : base()
+        {
+        }
+
+        /// <summary>
+        /// Calls methods SetUp() and Load().
+        /// Use this constructor when the object is in the database AND the connection string is in a config file
+        /// </summary>
+        /// <param name="key">ID number of a record in the database.
+        /// Sent as an arg to Load() to set values of record to properties of an 
+        /// object.</param>
+        public Customer(string key)
+            : base(key)
+        {
+        }
+
+        private Customer(CustomerProps props)
+            : base(props)
+        {
+        }
+
+        #endregion
     }
 }
+
